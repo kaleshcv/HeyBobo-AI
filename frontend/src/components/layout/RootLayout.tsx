9e196aaeb308fc6a40ac030e5496346a7451eacf
kalesh <kaleshcv2@gmail.com>
2026-03-24 18:43:51 +0530

    ui: remove header from public/auth layout

--- frontend/src/components/layout/RootLayout.tsx
+++ frontend/src/components/layout/RootLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from './Header'
 import { ErrorBoundary } from '@/components/common/ErrorBoundary'
 
 export default function RootLayout() {
   return (
     <ErrorBoundary>
       <div className="min-h-screen flex flex-col bg-white">
-        <Header />
         <main className="flex-1">
           <Outlet />
         </main>
